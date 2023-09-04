7b35c4cfdce91e2b1a214d6dfc3bda0de795f10d
owl4444 <malwaretable4444@gmail.com>
2023-09-04 07:43:05 +0000

    Edited BYOVD

---
+++ setup.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Check if commit message is provided
+if [ "$#" -ne 1 ]; then
+    echo "Error: You must provide a commit message."
+    exit 1
+fi
+
+# Commit message
+COMMIT_MSG="$1"
+
+# Build the Jekyll site
+echo "Building the Jekyll site..."
+bundle exec jekyll build
+
+if [ $? -ne 0 ]; then
+    echo "Error: Jekyll build failed."
+    exit 1
+fi
+
+# Add all changes to git
+echo "Staging changes..."
+git add -A
+
+# Commit with provided message
+echo "Committing with message: $COMMIT_MSG"
+git commit -m "$COMMIT_MSG"
+
+# Push to repository
+echo "Pushing to repository..."
+git push
+
+echo "Done!"
+
